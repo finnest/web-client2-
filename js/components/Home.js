ef46de8f47108ab8e5bf37f68be98808a70469d7
joeyoo <joseph_yoo@brown.edu>
2017-04-12 21:56:17 -0400

    edit css and add images

--- js/components/Home.js
+++ js/components/Home.js
@@ -6,15 +6,15 @@ const Home = React.createClass({
 		return (
 			<div className = 'home'>
 				<header>
-					<a href='/'><img src='public/img/1.png' /></a>
+					<a href='/'><img src='http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/v1/1084130/logo_old-01_swv6a5-2_znwimx.png' /></a>
 					<div>
 						<Link to='/login'>Sign In</Link>
 						<Link to='/register'>Sign Up</Link>
 					</div>
 				</header>
-				<div>
+				<div className='homepage-text'>
 					<h1>Learn Money. Start Saving.</h1>
-					<h2>Sign up tp become one of our early birds!</h2>
+					<h2>Sign up up become one of our early birds!</h2>
 					<div>
 						<Link to='/login'><button className='orangebutton'>Sign In</button></Link><br/>
 						<Link to='/register'><button>Sign Up</button></Link>
